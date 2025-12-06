69d93e84b681d6eed82f4b157c3b5224441486cd
Nome <exemploDeAtividade@gmail.com>
2025-12-06 18:33:38 -0300

    Adicionado funcionalidade salvar

---
+++ LeiloesTDSat/LeiloesTDSat/src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+
+    public static void main(String[] args) {
+        cadastroVIEW cv = new cadastroVIEW();
+        cv.setVisible(true);
+    }
+    
+}
--- LeiloesTDSat/LeiloesTDSat/src/ProdutosDAO.java
+++ LeiloesTDSat/LeiloesTDSat/src/ProdutosDAO.java
@@ -1,19 +1,9 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
-
-/**
- *
- * @author Adm
- */
-
 import java.sql.PreparedStatement;
 import java.sql.Connection;
 import javax.swing.JOptionPane;
 import java.sql.ResultSet;
 import java.util.ArrayList;
-
+import java.sql.SQLException;
 
 public class ProdutosDAO {
     
@@ -22,12 +12,36 @@ public class ProdutosDAO {
     ResultSet resultset;
     ArrayList<ProdutosDTO> listagem = new ArrayList<>();
     
-    public void cadastrarProduto (ProdutosDTO produto){
-        
-        
-        //conn = new conectaDAO().connectDB();
-        
-        
+    public void conectar(){
+        conectaDAO cd = new conectaDAO();
+        conn = cd.connectDB();
+    }
+    
+    public void desconectar(){
+        try{
+            conn.close();
+        }catch(SQLException ex){
+            
+        }
+    }
+    
+    public int cadastrarProduto (ProdutosDTO produto){
+        int status;
+        try{
+            conectar();
+            prep = conn.prepareStatement("INSERT INTO produtos (nome,valor,status) VALUES (?,?,?)");
+            prep.setString(1, produto.getNome());
+            prep.setInt(2, produto.getValor());
+            prep.setString(3, produto.getStatus());
+            status = prep.executeUpdate();
+            JOptionPane.showMessageDialog(null, "Cadastro feito");
+            desconectar();
+            return status;
+        }catch(SQLException ex){
+            System.out.println("Erro ao conecatar: "+ex);
+            JOptionPane.showMessageDialog(null, "Cadastro Falhou");
+            return ex.getErrorCode();
+        }
     }
     
     public ArrayList<ProdutosDTO> listarProdutos(){
